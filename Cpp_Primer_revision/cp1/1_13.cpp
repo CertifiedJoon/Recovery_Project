8a9cf8e1f477a1cc048092cc7e7168a1cdd0ef23
CertifiedJoon <u3566665@connect.hku.hk>
2021-03-06 04:18:11 +0000

    Bookstore problem started

--- Cpp_Primer_revision/cp1/1_13.cpp
+++ Cpp_Primer_revision/cp1/1_13.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 int main(){
-	int sum = 0;
-	for (int i = 50; i <= 100; ++i){
-		sum += i;
+	int i, j;
+	std::cin >> i >> j;
+	
+	for (; i < j; i++){
+		std::cout << i << std::endl;
 	}
 	
-	std::cout << sum << std::endl;
+	return 0;
 }
--- Cpp_Primer_revision/cp1/1_4.cpp
+++ Cpp_Primer_revision/cp1/1_4.cpp
@@ -4,7 +4,7 @@ int main(void){
 	int v1, v2;
 	
 	std::cin >> v1 >> v2;
-	std::cout << v1  << " * " << v2 << " = " << v1*v2 << endl;
+	std::cout << v1  << " * " << v2 << " = " << v1*v2 << std::endl;
 	
 	return 0;
 }
