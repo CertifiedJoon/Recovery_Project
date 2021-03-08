038b80b758ec044c4063d803444fe2d2f2dcb5a6
CertifiedJoon <u3566665@connect.hku.hk>
2021-03-08 11:00:28 +0000

    chapter 4 revision done

---
+++ Cpp_Primer_revision/cp4/4_28.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+	std::cout << sizeof(char) << std::endl;
+	std::cout << sizeof(short) << std::endl;
+	std::cout << sizeof(int) << std::endl;
+	std::cout << sizeof(unsigned) << std::endl;
+	std::cout << sizeof(long) << std::endl;
+	std::cout << sizeof(long long) << std::endl;
+	std::cout << sizeof(unsigned long long) << std::endl;
+	std::cout << sizeof(int *) << std::endl;
+	
+	return 0;
+}
