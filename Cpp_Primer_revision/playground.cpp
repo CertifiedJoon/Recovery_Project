f789fcc22aafe1060da15b193bb682ccc7b0573a
CertifiedJoon <u3566665@connect.hku.hk>
2021-03-14 02:03:47 +0000

    Just hanging in the playground

--- Cpp_Primer_revision/playground.cpp
+++ Cpp_Primer_revision/playground.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+void transform(std::string &word){
+	auto it = word.begin();
+	
+	while((it = find_if(it, word.end(), [](const char& c) { return std::ispunct(c); })) != word.end())
+		it = word.erase(it);
+	
+	for (auto &c : word)
+		c = tolower(c);
+}
 
 int main(){
-	int i = 50;
-	int sum = 0;
-	while (i <= 100){
-		sum += i++;
-	}
-	
-	std::cout << sum << std::endl;
+	std::string word ("h,ey,");
+	transform(word);
+	std::cout << word << std::endl;
 }
