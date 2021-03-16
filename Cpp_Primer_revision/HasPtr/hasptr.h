9584596ca4c2e79e8b538d2ab6da334de16e7ddd
CertifiedJoon <u3566665@connect.hku.hk>
2021-03-16 10:33:42 +0000

    weaved the fundamentals of the hasptr class

---
+++ Cpp_Primer_revision/HasPtr/hasptr.h
@@ -0,0 +1,16 @@
+#include <string>
+
+class HasPtr {
+public:
+	HasPtr(const std::string &s = std::string()): ps(new std::string(s)), i(0) {}
+	HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
+	HasPtr& operator= (const HasPtr &);
+private:
+	std::string *ps;
+	int i;
+}
+
+HasPtr& HasPtr::operator=(const HasPtr& rhs) 
+{
+	return (ps == rhs.ps) && (i == rhs.i);
+}
